931bf3eb85346d3b3c63eaee92d4615e03175672
Fernanda Maro <148138579+FernandaMaro@users.noreply.github.com>
2025-02-28 15:44:44 -0600

    simple layout

---
+++ src/componentes/Header.jsx
@@ -0,0 +1,10 @@
+import chefMaroLogo from "../assets/chef-maro-icon.png"
+
+export default function Header() {
+    return (
+        <header>
+            <img src={chefMaroLogo}/>
+            <h1>Chef Maro</h1>
+        </header>
+    )
+}
