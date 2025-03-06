b9880baa08d31a6840d1d1ddc3f191f4f9533388
Fernanda Maro <148138579+FernandaMaro@users.noreply.github.com>
2025-03-05 23:36:45 -0600

    API response

--- src/componentes/Hero.jsx
+++ src/componentes/Hero.jsx
@@ -1,40 +1,40 @@
-import React from "react"
+import React from "react";
+import IngredientsList from "./IngredientsList";
+import ChefMaro from "./ChefMaro";
+import { getRecipeFromChefClaude, getRecipeFromMistral } from "/ai";
 
-export default function Hero() {
+export default function Main() {
+  const [ingredients, setIngredients] = React.useState([]);
+  const [recipe, setRecipe] = React.useState("");
 
-    const [ingredients, setIngredients] = React.useState([])
+  async function getRecipe() {
+    const recipeMarkdown = await getRecipeFromMistral(ingredients);
+    setRecipe(recipeMarkdown);
+  }
 
-    const ingredientsListItems = ingredients.map(ingredient => (
-        <li key={ingredient}>{ingredient}</li>
-    ))
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  }
 
-    function addIngredient(formData) {
-        const newIngredient = formData.get("ingredient")
-        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
-    }
+  return (
+    <main>
+      <h2>Let's cook!</h2>
+      <form action={addIngredient} className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+          name="ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
 
-    return (
-        <main>
-            <form action={addIngredient} className="add-ingredient-form">
-                <input
-                    type="text"
-                    placeholder="e.g. oregano"
-                    aria-label="Add ingredient"
-                    name="ingredient"
-                />
-                <button>Add ingredient</button>
-            </form>
-            <section>
-                <h2>Ingredients on hand:</h2>
-                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-                <div className="get-recipe-container">
-                    <div>
-                        <h3>Ready for a recipe?</h3>
-                        <p>Generate a recipe from your list of ingredients.</p>
-                    </div>
-                    <button>Get a recipe</button>
-                </div>
-            </section>
-        </main>
-    )
+      {ingredients.length > 0 && (
+        <IngredientsList ingredients={ingredients} getRecipe={getRecipe} />
+      )}
+
+      {recipe && <ChefMaro recipe={recipe} />}
+    </main>
+  );
 }
